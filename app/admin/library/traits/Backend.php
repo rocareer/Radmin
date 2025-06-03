16d3fbdc9e980d9a2e5167cc22dc519816a92c11
albert <albert@rocareer.com>
2025-06-03 20:14:58 +0800

    refactor: 移除AiInterrupter类和Chat类，调整后台选择逻辑

--- app/admin/library/traits/Backend.php
+++ app/admin/library/traits/Backend.php
@@ -39,7 +39,7 @@ trait Backend
     {
 
         if ($this->request->input('select')) {
-            return $this->select();
+            // return $this->select();
         }
 
         list($where, $alias, $limit, $order) = $this->queryBuilder();
