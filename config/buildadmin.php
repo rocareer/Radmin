7507cb338a74bd6105ea9b7c6d677ebb2769e65d
albert <albert@rocareer.com>
2025-06-03 20:07:23 +0800

    chore: 更新依赖及配置，优化布局组件

--- config/buildadmin.php
+++ config/buildadmin.php
@@ -43,7 +43,7 @@ return [
         // 默认驱动方式
         'default' => 'mysql',
         // 加密key
-        'key'     => '06Jh5uNfTR4tYUsWxZGKrkwliHASFVp8',
+        'key'     => '0AkgIdacOU8nKMFNWbHjEt5oq4vyZm2L',
         // 加密方式
         'algo'    => 'ripemd160',
         // 驱动
