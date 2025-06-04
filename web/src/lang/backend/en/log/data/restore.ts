caf02a0583773b8a73fdf1c36443715df3c14075
albert <albert@rocareer.com>
2025-06-04 21:19:35 +0800

    chore: 删除项目中的配置、错误页面、数据库迁移和环境文件

--- web/src/lang/backend/en/log/data/restore.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    id: 'id',
-    admin_id: 'admin_id',
-    admin__username: 'username',
-    version: 'version',
-    table_name: 'table_name',
-    create_time: 'create_time',
-    'quick Search Fields': 'id',
-}
--- web/src/lang/backend/zh-cn/teset.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    id: 'ID',
-    status: '状态',
-    'status 0': '禁用',
-    'status 1': '启用',
-    file: '文件',
-    'quick Search Fields': 'ID',
-}
